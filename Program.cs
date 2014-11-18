cf16b4e42fb698aee369d7dde58d52ec39475107
Federico Javier Lopez Bertoni <federico@greenlight.com.py>
2014-11-18 16:49:58 -0300

    CracklePop initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CracklePop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i%15 == 0)
+                {
+                    Console.WriteLine("CracklePop");
+                }
+                else if (i%3 == 0)
+                {
+                    Console.WriteLine("Crackle");
+                }
+                else if (i%5 == 0)
+                {
+                    Console.WriteLine("Pop");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
